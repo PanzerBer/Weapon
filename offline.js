f9f661174614f2c45dc5c43eca10bb068e5edbc6
PanzerBer <83261745+PanzerBer@users.noreply.github.com>
2021-04-29 08:34:41 +0500

    Add files via upload

---
+++ offline.js
@@ -0,0 +1,17 @@
+﻿{
+	"version": 1619490702,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"images/wheel-sheet0.png",
+		"images/arrow-sheet0.png",
+		"images/button-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
